9b64b5c815774335590e6b954be36d1e7bae6ba6
shali <shali@DESKTOP-ESONBTR>
2021-03-15 14:16:01 +0530

    Add org

---
+++ NabhowareseleniumFramework/src/main/java/objectRepositoryUtility/Createneworg.java
@@ -0,0 +1,11 @@
+package objectRepositoryUtility;
+
+import org.testng.annotations.Test;
+
+public class Createneworg
+{
+   @Test
+   public void org(){
+	   System.out.println("Created org");
+   }
+}
