283e0f42d96d43e8df16c12818a5d3f5cd41b931
shali <shali@DESKTOP-ESONBTR>
2021-03-15 14:10:23 +0530

    Add new contact

---
+++ NabhowareseleniumFramework/src/main/java/objectRepositoryUtility/CreatenewContact.java
@@ -0,0 +1,12 @@
+package objectRepositoryUtility;
+
+import org.testng.annotations.Test;
+
+public class CreatenewContact
+{
+	@Test
+	public void newcontact(){
+		
+		System.out.println("contact created");
+	}
+}
