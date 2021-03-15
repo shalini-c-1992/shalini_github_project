eb1967856e9d3c7058ed01db62ff69700b91ac01
shali <shali@DESKTOP-ESONBTR>
2021-03-15 12:57:01 +0530

    New Framework

---
+++ NabhowareseleniumFramework/src/test/java/com/nabhoware/test/Sampleprogram.java
@@ -0,0 +1,23 @@
+package com.nabhoware.test;
+
+import org.testng.annotations.Test;
+
+public class Sampleprogram 
+{
+	@Test
+	public void Createcontacttest(){
+		
+		System.out.println("Execute create contact");
+	}
+	
+	@Test
+	public void Modifycontacttest(){
+		
+		System.out.println("Exececute Modify contact ");
+	}
+	
+	@Test
+	public void Deletecontacttest(){
+		System.out.println("Exececute Delete contact");
+	}
+}
